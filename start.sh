5098119102f63adf22f65547f1337b2d72b9e597
Adriano Pezzuto <kalise@users.noreply.github.com>
2017-02-10 18:47:54 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-
-# Customize tomcat/webapps and tomcat/logs directories
+# Customize /opt/tomcat/{webapps,logs} directories
 # based on the user and instance names
-mkdir -p /mnt/$USER_NAME/$INSTANCE_NAME/{webapps,logs}
+mkdir -p /mnt/$USER_NAME/$INSTANCE_NAME
+mv /opt/tomcat/{webapps,logs} /mnt/$USER_NAME/$INSTANCE_NAME/{webapps,logs}
 chmod g+rwx /mnt/$USER_NAME/$INSTANCE_NAME/{webapps,logs}
 rm -rf /opt/tomcat/{webapps,logs}
 ln -s /mnt/$USER_NAME/$INSTANCE_NAME/webapps /opt/tomcat/webapps
 ln -s /mnt/$USER_NAME/$INSTANCE_NAME/logs /opt/tomcat/logs
-
-# Start the Tomcat
-/opt/tomcat/bin/catalina.sh run
